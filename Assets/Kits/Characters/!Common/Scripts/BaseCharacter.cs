5d82638e9e04eb6a486f51d7b48863fca64ee840
AortizVale <aortiz449@unab.edu.co>
2026-02-18 14:10:34 -0500

    Update BaseCharacter.cs

--- Assets/Kits/Characters/!Common/Scripts/BaseCharacter.cs
+++ Assets/Kits/Characters/!Common/Scripts/BaseCharacter.cs
@@ -10,7 +10,7 @@ public class BaseCharacter : MonoBehaviour, IVisible2D
     [SerializeField] protected Life life;
 
     [Header("Directional Attack")]
-    [SerializeField] IVisible2D.Side[] sidesToAttrack = { IVisible2D.Side.PlayerFriends };
+    [SerializeField] IVisible2D.Side[] sidesToAttack = { IVisible2D.Side.PlayerFriends };
     [SerializeField] protected float attackRange = 1f;
     [SerializeField] protected float attackRadius = 0.3f;
     [SerializeField] protected float attackDamage = 0.2f;
@@ -100,7 +100,7 @@ public class BaseCharacter : MonoBehaviour, IVisible2D
 
                     bool canBeAttacked = false;
 
-                    foreach (var side in sidesToAttrack)
+                    foreach (var side in sidesToAttack)
                     {
                         if (side == otherSide)
                         {
